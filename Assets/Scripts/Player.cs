0c4c0dcd248e4b37db6b85034c9a91cd8db0e67f
HobieDog <57800848+HobieDog@users.noreply.github.com>
2020-05-29 18:37:08 +0900

    Add Player Move & Remove Ground Friction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,7 +19,16 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        throw new System.NotImplementedException();
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(health.fillAmount);
+        }
+        else
+        {
+            curPos = (Vector3)stream.ReceiveNext();
+            health.fillAmount = (float)stream.ReceiveNext();
+        }
     }
 
     void Awake()
@@ -31,6 +40,34 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
 
     void Update()
     {
-        
+        if (photonV.IsMine)
+        {
+            //← → Move
+            float h = Input.GetAxisRaw("Horizontal");
+            rigid.velocity = new Vector2(4 * h, rigid.velocity.y);
+
+            if (h != 0)
+            {
+                anim.SetBool("walk", true);
+                photonV.RPC("FlipXRPC", RpcTarget.AllBuffered, h); // 재접속시 filpX를 동기화해주기 위해서 AllBuffered
+            }
+            else anim.SetBool("walk", false);
+
+            // ↑ 점프, 바닥체크
+            isGround = Physics2D.OverlapCircle((Vector2)transform.position + new Vector2(0, -0.5f), 0.07f, 1 << LayerMask.NameToLayer("Ground"));
+            anim.SetBool("jump", !isGround);
+            if ((Input.GetKeyDown(KeyCode.UpArrow) && isGround) || (Input.GetKeyDown(KeyCode.Space) && isGround))
+                photonV.RPC("JumpRPC", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    void FlipXRPC(float h) => spriteRenderer.flipX = h == -1;
+
+    [PunRPC]
+    void JumpRPC()
+    {
+        rigid.velocity = Vector2.zero;
+        rigid.AddForce(Vector2.up * 600);
     }
 }
